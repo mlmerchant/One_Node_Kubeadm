ce44ad48b732e19ebdb968419009f9351ce195f5
Matt Merchant <matthew.lee.merchant@gmail.com>
2023-07-22 17:48:51 -0500

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,66 @@
+#!/bin/bash
+# Install a single node kubernetes cluster using kubeadm on Ubuntu 22.04
+
+# Forwrding IPv4 and letting iptables see bridged traffic
+# https://kubernetes.io/docs/setup/production-environment/container-runtimes/
+cat <<EOF | sudo tee /etc/modules-load.d/k8s.conf
+overlay
+br_netfilter
+EOF
+sudo modprobe overlay
+sudo modprobe br_netfilter
+# sysctl params required by setup, params persist across reboots
+cat <<EOF | sudo tee /etc/sysctl.d/k8s.conf
+net.bridge.bridge-nf-call-iptables  = 1
+net.bridge.bridge-nf-call-ip6tables = 1
+net.ipv4.ip_forward                 = 1
+EOF
+# Apply sysctl params without reboot
+sudo sysctl --system
+
+
+# Install containerd
+# https://docs.docker.com/engine/install/ubuntu/
+sudo apt-get update
+sudo apt-get install ca-certificates curl gnupg
+sudo install -m 0755 -d /etc/apt/keyrings
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+sudo chmod a+r /etc/apt/keyrings/docker.gpg
+echo \
+  "deb [arch="$(dpkg --print-architecture)" signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
+  "$(. /etc/os-release && echo "$VERSION_CODENAME")" stable" | \
+  sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+sudo apt-get update
+sudo apt-get install containerd.io
+
+
+# Install latest kubeadm, kubelet, and kubectl
+# https://kubernetes.io/docs/setup/production-environment/tools/kubeadm/install-kubeadm/
+sudo mkdir -p /etc/apt/keyrings
+sudo apt-get update
+sudo apt-get install -y apt-transport-https ca-certificates curl
+echo "deb [signed-by=/etc/apt/keyrings/kubernetes-archive-keyring.gpg] https://apt.kubernetes.io/ kubernetes-xenial main" | sudo tee /etc/apt/sources.list.d/kubernetes.list
+sudo apt-get update
+sudo apt-get install -y kubelet kubeadm kubectl
+sudo apt-mark hold kubelet kubeadm kubectl
+
+
+# Run kubeadm init
+# https://kubernetes.io/docs/reference/setup-tools/kubeadm/kubeadm-init/
+kubeadm init --apiserver-advertise-address=$(hostname -I | awk '{print $1}')
+
+
+# Untaint the Master Node
+# https://stackoverflow.com/questions/43147941/allow-scheduling-of-pods-on-kubernetes-master
+kubectl taint nodes --all node-role.kubernetes.io/master-
+
+
+# Make kubectl work for non root user:
+mkdir -p $HOME/.kube
+sudo cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
+sudo chown $(id -u):$(id -g) $HOME/.kube/config
+
+
+# Install the network add-on:
+# https://www.weave.works/docs/net/latest/kubernetes/kube-addon/
+kubectl apply -f https://github.com/weaveworks/weave/releases/download/v2.8.1/weave-daemonset-k8s.yaml
