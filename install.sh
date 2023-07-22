03bbd7af6486ffcefc20be8b9cd25dfefd59adc2
Matt Merchant <matthew.lee.merchant@gmail.com>
2023-07-22 18:32:52 -0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -38,6 +38,7 @@ sudo apt-get install containerd.io
 # Install latest kubeadm, kubelet, and kubectl
 # https://kubernetes.io/docs/setup/production-environment/tools/kubeadm/install-kubeadm/
 sudo mkdir -p /etc/apt/keyrings
+curl -fsSLo /etc/apt/keyrings/kubernetes-archive-keyring.gpg https://packages.cloud.google.com/apt/doc/apt-key.gpg
 sudo apt-get update
 sudo apt-get install -y apt-transport-https ca-certificates curl
 echo "deb [signed-by=/etc/apt/keyrings/kubernetes-archive-keyring.gpg] https://apt.kubernetes.io/ kubernetes-xenial main" | sudo tee /etc/apt/sources.list.d/kubernetes.list
