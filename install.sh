560c3806ec97a70f56824dac50d05d97d3747329
Matt Merchant <matthew.lee.merchant@gmail.com>
2023-07-22 21:16:36 -0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -61,7 +61,7 @@ sudo cp /etc/kubernetes/admin.conf $HOME/.kube/config
 sudo chown $(id -u):$(id -g) $HOME/.kube/config
 
 
-#seconds=10
+seconds=10
 echo Sleeping for $seconds seconds.
 sleep $seconds
 
