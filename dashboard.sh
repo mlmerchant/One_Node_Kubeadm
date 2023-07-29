1dfaf82ceb6e09c80f56d2d12d51bc11cc754185
Matt Merchant <matthew.lee.merchant@gmail.com>
2023-07-29 10:51:29 -0500

    Create dashboard.sh

---
+++ dashboard.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# Add kubernetes-dashboard repository
+helm repo add kubernetes-dashboard https://kubernetes.github.io/dashboard/
+
+# Deploy a Helm Release named "kubernetes-dashboard" using the kubernetes-dashboard chart
+helm upgrade --install kubernetes-dashboard kubernetes-dashboard/kubernetes-dashboard --create-namespace --namespace kubernetes-dashboard > install.log 2>&1
